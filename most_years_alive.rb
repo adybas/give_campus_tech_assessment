2f93195bdfcc86f025c6389174fbca25a9c04c89
Anna Dybas <a.m.dybas@gmail.com>
2020-08-31 19:37:13 -0400

    added rspec testing for most_years_alive

--- most_years_alive.rb
+++ most_years_alive.rb
@@ -4,14 +4,14 @@ def most_years_alive(arr)
     arr.each do |years|
       years_range = Array (years[0]..years[1])
       years_range.each do |year|
-        years_alive_count_hash[year]? years_alive_count_hash[year] += 1 : years_alive_count_hash[year] = 1
+        years_alive_count_hash[year] ? years_alive_count_hash[year] += 1 : years_alive_count_hash[year] = 1
       end
     end
 
-    return find_max_values(years_alive_count_hash)
+    return find_keys_of_max_values(years_alive_count_hash)
 end
 
-def find_max_value_keys(years_alive_hash)
+def find_keys_of_max_values(years_alive_hash)
    maxAlive = years_alive_hash.values.max
    return years_alive_hash.select { |year, count| count == maxAlive }.keys
 end
